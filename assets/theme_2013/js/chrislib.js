8fc9c1f12696d18cd73b98a51124dc2857c50ee5
chris <christophercuizons@gmail.com>
2013-10-18 15:19:40 +0800

    Updated sqls

--- assets/theme_2013/js/chrislib.js
+++ assets/theme_2013/js/chrislib.js
@@ -56,20 +56,7 @@ var kpay = {
 
 // overwrite comments
 window.alert = function(msg){
-<<<<<<< HEAD
-   jQuery(".source_error").html(msg).dialog({
-	   	draggable: false,
-	   	resizable: false,
-	   	modal: true,
-	   	width: 'inherit',
-	   	dialogClass:'transparent',
-	   	overlay: {
-   	    	opacity: 0
-   	    },
-	    open : function() {
-           jQuery('.source_error').dialog("option", "title" ,"Error");
-     	}
-=======
+
    jQuery(".source_error").html(msg);
    jQuery(".source_error").dialog({
 	   width: 'inherit',
@@ -82,7 +69,6 @@ window.alert = function(msg){
 	   overlay: {
    		   opacity: 0
    	   }
->>>>>>> 0901721c83f32782c4105126a1da4909b1dce271
    });
 }
 
