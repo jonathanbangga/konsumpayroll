bf0d4e4083a82724a7201be89b8b7600585a0d5b
jonathan <jonathanbangga@gmail.com>
2013-10-23 11:15:39 +0800

    updated file

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,7 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-<<<<<<< HEAD
 class Welcome extends CI_Controller {
 	
 	public function __construct() {
@@ -10,31 +9,6 @@ class Welcome extends CI_Controller {
 
 	public function test(){
 		echo "hello";
-=======
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
-		//echo $a+$b;
-	}
-	
-	public function test($a,$b){
-		echo $a+$b;
->>>>>>> ffdc59abc5f683ceaeb48d0edcacd562ab63872b
 	}
 	
 	
