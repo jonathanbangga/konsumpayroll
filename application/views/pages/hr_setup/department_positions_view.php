bda11fc7e9fee3988e46bab83aaafda064d59be3
jc <vaultdweller123@gmail.com>
2013-11-04 10:16:54 +0800

    updates

---
+++ application/views/pages/hr_setup/department_positions_view.php
@@ -0,0 +1,142 @@
+<div class="main-content">
+        <!-- MAIN-CONTENT START -->
+        <p>To generate payroll reports by department, select the department from the list and click arrow right otherwise<br>
+          add department if it still doesn't exist.</p>
+        <div class="tbl-wrap department-wrap">
+          <!-- TBL-WRAP START -->
+          <ul>
+            <li class="main-department">
+              <header>Departments</header>
+              <div class="dept-box">
+                <ul>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      Accounting</label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      Administration</label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      HR</label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      Marketing</label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      Operations</label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      Sales</label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      Development</label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      Call Center</label>
+                  </li>
+                </ul>
+              </div>
+              <a class="btn" href="#">ADD DEPARTMENT</a> </li>
+            <li class="sub-administration">
+              <header>Administration</header>
+              <div class="dept-box">
+                <ul>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      Admin Staff</label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      Security Personnel</label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      System Administrator</label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      Maintenance</label>
+                  </li>
+                </ul>
+              </div>
+              <a class="btn" href="#">ADD POSITION</a> </li>
+            <li class="sub-hr">
+              <header>HR</header>
+              <div class="dept-box">
+                <ul>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      HR Administrator</label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      HR Assistant</label>
+                  </li>
+                </ul>
+              </div>
+              <a class="btn" href="#">ADD POSITION</a> </li>
+            <li class="sub-departments nomar-btm">
+              <header>Departments</header>
+              <div class="dept-box">
+                <ul>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      Sr. Software Engineer</label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      Jr. Software Engineer<br>
+                    </label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      CSS Developer</label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      Graphic Artist</label>
+                  </li>
+                  <li>
+                    <label>
+                      <input class="right" name="" type="checkbox" value="">
+                      QA Specialist</label>
+                  </li>
+                </ul>
+              </div>
+              <a class="btn" href="#">ADD POSITION</a> </li>
+          </ul>
+          <div class="clearB"></div>
+          <!-- TBL-WRAP END -->
+        </div>
+        <!-- MAIN-CONTENT END -->
+      </div>
+      <div class="footer-grp-btn">
+        <!-- FOOTER-GRP-BTN START -->
+        <a class="btn btn-gray left" href="#">BACK</a> <a class="btn btn-gray right" href="#">CONTINUE</a>
+        <!-- FOOTER-GRP-BTN END -->
+      </div>
