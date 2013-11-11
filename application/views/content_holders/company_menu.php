f63a928fed66672f8c3c1683a93acfecc872b7e7
jc <vaultdweller123@gmail.com>
2013-11-11 15:20:00 +0800

    wrap

--- application/views/content_holders/company_menu.php
+++ application/views/content_holders/company_menu.php
@@ -1,6 +1,6 @@
       <ul>
-        <li><a href="/">COMPANY SETUP</a></li>
-        <li><a href="/tgg/hr_setup/employment_type">HR SETUP</a></li>
+        <li><a href="/company/company_setup/company_information">COMPANY SETUP</a></li>
+        <li><a href="/company/hr_setup/employment_type">HR SETUP</a></li>
         <li><a href="#">PAYROLL SETUP</a></li>
       </ul>
      
