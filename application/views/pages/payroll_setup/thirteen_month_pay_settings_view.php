fcdd8d0f5bccde76d781684d4103458142481acd
jc <vaultdweller123@gmail.com>
2013-11-29 09:47:27 +0800

    moved some css from global.css to my custom css, jc.css

---
+++ application/views/pages/payroll_setup/thirteen_month_pay_settings_view.php
@@ -0,0 +1,94 @@
+<div class="main-content">
+        <!-- MAIN-CONTENT START -->
+        <p>Identify the income to be included in 13th month pay. </p>
+        <div class="tbl-wrap">
+          <!-- TBL-WRAP START -->
+          <table class="tbl">
+            <tr>
+              <th style="width:135px;">Income</th>
+              <th style="width:135px">Include</th>
+            </tr>
+            <tr>
+              <td>Basic Pay</td>
+              <td><select style="width:115px;" class="txtselect" name="">
+                </select></td>
+            </tr>
+            <tr>
+              <td>Overtime</td>
+              <td><select style="width:115px;" class="txtselect" name="">
+                </select></td>
+            </tr>
+            <tr>
+              <td>Holiday/Premium Pay</td>
+              <td><select style="width:115px;" class="txtselect" name="">
+                </select></td>
+            </tr>
+            <tr>
+              <td>Night Shift Differential</td>
+              <td><select style="width:115px;" class="txtselect" name="">
+                </select></td>
+            </tr>
+          </table>
+          <!-- TBL-WRAP END -->
+        </div>
+        <p> Identify the income to be included in 13th month pay.</p>
+        <table style="margin-left:10px;" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td style="width:150px;" ><input style="margin-right:5px;" type="radio" name="radiobtn1" value="">
+              Current Basic Pay</td>
+            <td style="width:170px;"><input style="margin-right:5px;" type="radio" name="radiobtn1" value="">
+              Average annual basic pay</td>
+          </tr>
+        </table>
+        <br>
+        <br>
+        <p>dentify the other earnings to be included in 13th month pay.</p>
+        <div class="tbl-wrap">
+          <!-- TBL-WRAP START -->
+          <table class="tbl">
+            <tr>
+              <th style="width:135px;">Other Earnings</th>
+              <th style="width:135px">Include</th>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+              <td><select style="width:115px;" class="txtselect" name="">
+                </select></td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+              <td><select style="width:115px;" class="txtselect" name="">
+                </select></td>
+            </tr>
+          </table>
+          <!-- TBL-WRAP END -->
+        </div>
+        <p> Select whether to include tardiness, absences, or under time as deduction from the total employee income</p>
+        <div class="tbl-wrap">
+          <!-- TBL-WRAP START -->
+          <table class="tbl">
+            <tr>
+              <th style="width:135px;">Other Earnings</th>
+              <th style="width:135px">Include</th>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+              <td><select style="width:115px;" class="txtselect" name="">
+                </select></td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+              <td><select style="width:115px;" class="txtselect" name="">
+                </select></td>
+            </tr>
+          </table>
+          <!-- TBL-WRAP END -->
+        </div>
+        <!-- MAIN-CONTENT END -->
+      </div>
+      <div class="footer-grp-btn">
+        <!-- FOOTER-GRP-BTN START -->
+        <a class="btn btn-gray left" href="#">BACK</a> <a class="btn btn-gray right" href="#"> CONTINUE</a>
+        <input style="margin-right:10px;" class="btn right" name="" type="button" value="SAVE">
+        <!-- FOOTER-GRP-BTN END -->
+      </div>
