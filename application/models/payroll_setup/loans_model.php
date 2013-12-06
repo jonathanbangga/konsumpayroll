f3ca6a82063f3c7d1260b19e3d7fa3d9f91995aa
jc <vaultdweller123@gmail.com>
2013-12-06 14:09:12 +0800

    added loan, complete with ajax edit, edit and delete

--- application/models/payroll_setup/loans_model.php
+++ application/models/payroll_setup/loans_model.php
@@ -9,6 +9,47 @@ class Loans_model extends CI_Model {
 		// default
 		$this->company_id = $this->session->userdata('company_id');
     }
+	
+	public function get_loans(){
+		return $this->db->query("
+			SELECT *
+			FROM `loan_type`
+			WHERE `company_id` = {$this->company_id}
+		");
+	}
+	
+	public function add_loans($loan_type_name){
+		$this->db->query("
+			INSERT INTO 
+			`loan_type` (
+				`loan_type_name`,
+				`company_id`
+			)
+			VALUES (
+				'{$loan_type_name}',
+				'{$this->company_id}'
+			)
+		");
+	}
+	
+	public function delete_loans($loan_type_id){
+		$loan_type_id2 = implode(",",$loan_type_id);
+		$this->db->query("
+			DELETE
+			FROM `loan_type`
+			WHERE `loan_type_id` IN({$loan_type_id2})
+			AND `company_id` = {$this->company_id}
+		");
+	}
+	
+	public function update_loans($loan_type_id,$loan_type_name){
+		$this->db->query("
+			UPDATE `loan_type`
+			SET `loan_type_name` = '{$loan_type_name}'
+			WHERE `loan_type_id` = {$loan_type_id}
+			AND `company_id` = {$this->company_id}
+		");
+	}
 		
 }
 /* End of file */
