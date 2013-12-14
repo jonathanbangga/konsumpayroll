87109097c2b9389c2ad9f4b3d3b6379b16c84ff6
Joe Christian Alcomendras <vaultdweller123@gmail.com>
2013-12-14 18:59:14 +0800

    completed payroll setup's payroll calendar, ajax listing add ang update

--- application/models/payroll_setup/payroll_calendar_model.php
+++ application/models/payroll_setup/payroll_calendar_model.php
@@ -42,5 +42,37 @@ class Payroll_calendar_model extends CI_Model {
 		");
 	}
 	
+	public function get_distinct_year($payroll_group_id){
+		return $this->db->query("
+			SELECT DISTINCT YEAR(  `payroll_date` ) AS payroll_year
+			FROM  `payroll_calendar` 
+			WHERE  `payroll_group_id` ={$payroll_group_id}
+			AND  `company_id` ={$this->company_id}
+		");
+	}
+	
+	public function get_payroll_calendar($payroll_group_id,$year){
+		return $this->db->query("
+			SELECT * 
+			FROM  `payroll_calendar` 
+			WHERE YEAR(  `payroll_date` ) =  '{$year}'
+			AND  `payroll_group_id` ={$payroll_group_id}
+			AND  `company_id` ={$this->company_id}
+		");
+	}
+	
+	public function update_payroll_calendar($payroll_calendar_id,$payroll_date="",$cut_off_from="",$cut_off_to="",$period=""){
+		$this->db->query("
+			UPDATE `payroll_calendar` 
+			SET
+				`payroll_date` = '{$payroll_date}',
+				`cut_off_from` = '{$cut_off_from}',
+				`cut_off_to` = '{$cut_off_to}',
+				`period` = '{$period}'
+			WHERE  `payroll_calendar_id` ={$payroll_calendar_id} 
+			AND  `company_id` ={$this->company_id}
+		");
+	}
+	
 }
 /* End of file */
--- application/views/pages/payroll_setup/payroll_calendar_view.php
+++ application/views/pages/payroll_setup/payroll_calendar_view.php
@@ -1,4 +1,8 @@
 <div class="main-content">
+<?php 
+$attributes = array('id' => 'jform');
+echo form_open("/{$this->session->userdata('sub_domain')}/payroll_setup/payroll_calendar",$attributes);
+?>
 <div style="display:none;" class="highlight_message">Message</div>
         <!-- MAIN-CONTENT START -->
         <p> Define the dates of the payroll to each of your payroll group. Make sure you enter the correct date range.</p>
@@ -10,13 +14,13 @@
 			  <!--PAYROLL-CALENDAR-ROW START -->
 			  <h5>
 				<?php echo $pg->name ?>
-				<input type="hidden" class="pg_id" value="<?php echo $pg->payroll_group_setup_id; ?>" />
+				<input type="hidden" class="pg_id" name="pg_id[]" value="<?php echo $pg->payroll_group_setup_id; ?>" />
 			  </h5>
 			  <table style="margin-bottom:8px;">
 				<tr>
 				  <td style="width:314px;"> Indicate the date of the first semi-monthly payroll </td>
 				  <td style="width:120px;">
-					<select style="width:120px;" class="txtselect semi_monthly">
+					<select style="width:120px;" class="txtselect semi_monthly" name="semi_monthly[]">
 						<option value="-1">select</option>
 						<?php
 						for($i=1;$i<=15;$i++){?>
@@ -30,7 +34,7 @@
 				<tr>
 				  <td>Indicate the date of your second monthly payroll</td>
 				  <td>
-					<select style="width:120px;" class="txtselect monthly">
+					<select style="width:120px;" class="txtselect monthly" name="monthly[]">
 						<option value="-1">select</option>
 						<?php
 						for($i=16;$i<=31;$i++){?>
@@ -44,17 +48,17 @@
 				</tr>
 			  </table>
 			  <p style="padding-bottom:8px;">State the first payroll for this group that will be run by this system
-				<input type="text" class="txtfield dp payroll_date" style="width:120px;" />
+				<input type="text" class="txtfield dp payroll_date" name="payroll_date[]" style="width:120px;" />
 			  </p>
 			  <table border="0" cellspacing="0" cellpadding="0">
 				<tr>
 				  <td style="width:512px;">Select range of work days to be included in first payroll for this group using the system</td>
-				  <td style="width:180px;"><input class="txtfield dp cut_off_from" style="width:70px;" type="text">
-					<input class="txtfield dp cut_off_to" style="width:70px;" type="text"></td>
+				  <td style="width:180px;"><input class="txtfield dp cut_off_from" name="cut_off_from[]" style="width:70px;" type="text">
+					<input class="txtfield dp cut_off_to" name="cut_off_to[]" style="width:70px;" type="text"></td>
 				</tr>
 				<tr>
 				  <td colspan="2">
-				  <a class="btn" id="save" href="javascript:void(0);" id="add-project" >SAVE</a>
+				  <a class="btn save" href="javascript:void(0);" id="add-project" >SAVE</a>
 				  <a class="btn right show_calendar" href="javascript:void(0);">SHOW CALENDAR</a>
 					<div class="clearB"></div>
 				</td>
@@ -68,9 +72,11 @@
 			echo 'No Payroll Group created. click <a href="/'.$this->session->userdata('sub_domain').'/payroll_setup/payroll_group">here</a>';
 		}
 		?>
-        
+		
+		<input type="submit" class="btn" id="save_all" name="save_all" value="Save All" />
       
         <!-- MAIN-CONTENT END -->
+		<?php echo form_close();?>
       </div>
       <div class="footer-grp-btn">
         <!-- FOOTER-GRP-BTN START -->
@@ -80,31 +86,16 @@
 	  
 <div id="payroll_calendar_dialog" class="jdialog"  title="Payroll Calendar">
 	<div class="inner_div">
+		<div style="display:none;" class="highlight_message msg2">Message</div>
 		<div style="float:right;margin-bottom: 15px;">
 		Year 
-		<select class="txtselect" style="width: 80px;">
-			<option>2013</option>
-		</select>
+		<span id="pgy_span">
+		</span>
 		</div>
 		<div style="clear:both;"></div>
-		<table>
-			<thead>
-				<tr>
-					<th>Payroll Date</th>
-					<th>From</th>
-					<th>To</th>
-					<th>Period</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td><input class="txtfield dp" type="text" value="2013-12-12" /></td>
-					<td><input class="txtfield dp" type="text" value="2013-12-12" /></td>
-					<td><input class="txtfield dp" type="text" value="2013-12-12" /></td>
-					<td><input class="txtfield" type="text" value="12" style="width: 20px;" /></td>
-				</tr>
-			</tbody>
-		</table>
+		<div id="pc_table_div">
+		Please Select Year
+		</div>
 	</div>
 </div>
 
@@ -133,45 +124,188 @@ jQuery(document).ready(function(){
 	// invoke date picker
 	jQuery( ".dp" ).datepicker();
 	
-	jQuery("#save").click(function(){
+	jQuery(".save").click(function(){
+		var obj = jQuery(this);
+		var pg_id = obj.parents(".payroll-calendar-row").find(".pg_id").val();
+		var semi_monthly = obj.parents(".payroll-calendar-row").find(".semi_monthly").val();
+		var monthly = obj.parents(".payroll-calendar-row").find(".monthly").val();
+		var payroll_date = obj.parents(".payroll-calendar-row").find(".payroll_date").val();
+		var cut_off_from = obj.parents(".payroll-calendar-row").find(".cut_off_from").val();
+		var cut_off_to = obj.parents(".payroll-calendar-row").find(".cut_off_to").val();
+		var error = "";
+		if(semi_monthly==-1){
+			error += "Semi month payroll date is required<br />";
+		}
+		if(monthly==-1){
+			error += "Monthly payroll date is required<br />";
+		}
+		if(payroll_date==""){
+			error += "Payroll date is required<br />";
+		}
+		if(cut_off_from==""){
+			error += "Cut off from date is required<br />";
+		}
+		if(cut_off_to==""){
+			error += "Cut off to date is required<br />";
+		}
+		if(error==""){
+			
+			jQuery.ajax({
+				type: "POST",
+				url: "/<?php echo $this->session->userdata('sub_domain'); ?>/payroll_setup/payroll_calendar/ajax_add_payroll_calendar",
+				data: {
+					pg_id: pg_id,
+					semi_monthly: semi_monthly,
+					monthly: monthly,
+					payroll_date: payroll_date,
+					cut_off_from: cut_off_from,
+					cut_off_to: cut_off_to,
+					<?php echo itoken_name();?>: jQuery.cookie("<?php echo itoken_cookie(); ?>")
+				}
+			}).done(function(ret){
+				obj.parents(".payroll-calendar-row").find(".semi_monthly").val("");
+				obj.parents(".payroll-calendar-row").find(".monthly").val("");
+				obj.parents(".payroll-calendar-row").find(".payroll_date").val("");
+				obj.parents(".payroll-calendar-row").find(".cut_off_from").val("");
+				obj.parents(".payroll-calendar-row").find(".cut_off_to").val("");
+				highlight_message("Payroll calendar has been saved")
+			});	
+			
+		}else{
+			alert(error)
+		}
+			
+	});
+	
+	jQuery("#save_all").click(function(){
+		var is_empty = false;
+		jQuery(".semi_monthly").each(function(){
+			if(jQuery(this).val()==-1){
+				is_empty = true;
+			}
+		});
+		jQuery(".monthly").each(function(){
+			if(jQuery(this).val()==-1){
+				is_empty = true;
+			}
+		});
+		jQuery(".payroll_date").each(function(){
+			if(jQuery(this).val()==""){
+				is_empty = true;
+			}
+		});
+		jQuery(".cut_off_from").each(function(){
+			if(jQuery(this).val()==""){
+				is_empty = true;
+			}
+		});
+		jQuery(".cut_off_to").each(function(){
+			if(jQuery(this).val()==""){
+				is_empty = true;
+			}
+		});
+		if(is_empty){
+			alert("all field are required");
+		}else{
+			jQuery("#jform").submit();
+		}
+	});
+	
+	// show caledar
+	jQuery(".show_calendar").click(function(){
 		var pg_id = jQuery(this).parents(".payroll-calendar-row").find(".pg_id").val();
-		var semi_monthly = jQuery(this).parents(".payroll-calendar-row").find(".semi_monthly").val();
-		var monthly = jQuery(this).parents(".payroll-calendar-row").find(".monthly").val();
-		var payroll_date = jQuery(this).parents(".payroll-calendar-row").find(".payroll_date").val();
-		var cut_off_from = jQuery(this).parents(".payroll-calendar-row").find(".cut_off_from").val();
-		var cut_off_to = jQuery(this).parents(".payroll-calendar-row").find(".cut_off_to").val();
 		jQuery.ajax({
 			type: "POST",
-			url: "/<?php echo $this->session->userdata('sub_domain'); ?>/payroll_setup/payroll_calendar/ajax_add_payroll_calendar",
+			url: "/<?php echo $this->session->userdata('sub_domain'); ?>/payroll_setup/payroll_calendar/ajax_get_payroll_calendar_year",
 			data: {
 				pg_id: pg_id,
-				semi_monthly: semi_monthly,
-				monthly: monthly,
-				payroll_date: payroll_date,
-				cut_off_from: cut_off_from,
-				cut_off_to: cut_off_to,
 				<?php echo itoken_name();?>: jQuery.cookie("<?php echo itoken_cookie(); ?>")
 			}
 		}).done(function(ret){
-			jQuery.cookie("msg", "Payroll calendar has been saved");
-			window.location="/<?php echo $this->session->userdata('sub_domain'); ?>/payroll_setup/payroll_calendar";
-		});		
+			jQuery("#pgy_span").html(ret);
+			jQuery("#payroll_calendar_dialog").dialog({
+				modal: true,
+				show: {
+					effect: "blind"
+				},
+				close: function( event, ui ) {
+					jQuery("#pc_table_div").html("Please Select Year");
+				},
+				buttons: {
+					'save': function() {
+						// ajax call
+								var is_changed = new Array();
+								jQuery("#payroll_calendar_dialog .is_changed").each(function(index){
+									is_changed[index] = jQuery(this).val();
+								});
+								var pc_id = new Array();
+								jQuery("#payroll_calendar_dialog .pc_id").each(function(index){
+									pc_id[index] = jQuery(this).val();
+								});
+								var payroll_date = new Array();
+								jQuery("#payroll_calendar_dialog .edit_payroll_date").each(function(index){
+									payroll_date[index] = jQuery(this).val();
+								});
+			
+								var cut_off_from = new Array();
+								jQuery("#payroll_calendar_dialog .edit_cut_off_from").each(function(index){
+									cut_off_from[index] = jQuery(this).val();
+								});
+								var cut_off_to = new Array();
+								jQuery("#payroll_calendar_dialog .edit_cut_off_to").each(function(index){
+									cut_off_to[index] = jQuery(this).val();
+								});
+								var period = new Array();
+								jQuery("#payroll_calendar_dialog .period").each(function(index){
+									period[index] = jQuery(this).val();
+								});
+								jQuery.ajax({
+									type: "POST",
+									url: "/<?php echo $this->session->userdata('sub_domain'); ?>/payroll_setup/payroll_calendar/ajax_update_payroll_calendar",
+									data: {
+										is_changed: is_changed,
+										pc_id: pc_id,
+										payroll_date: payroll_date,
+										cut_off_from: cut_off_from,
+										cut_off_to: cut_off_to,
+										period: period,
+										<?php echo itoken_name();?>: jQuery.cookie("<?php echo itoken_cookie(); ?>")
+									}
+								}).done(function(ret){
+									highlight_message("Changes has been saved",".msg2");
+								});	
+							
+					
+						
+					}
+				}
+			});
+		});	
 	});
 	
-	jQuery(".show_calendar").click(function(){
-		
-		jQuery("#payroll_calendar_dialog").dialog({
-			modal: true,
-			show: {
-				effect: "blind"
-			},
-			buttons: {
-				'save': function() {
-					// ajax call
-							
-				}
+	// select year
+	jQuery(document).on("change","#pcy_select",function(){
+		var pg_id = jQuery("#pg_id").val();
+		var year = jQuery(this).val();
+		console.log(pg_id+" "+year);
+		jQuery.ajax({
+			type: "POST",
+			url: "/<?php echo $this->session->userdata('sub_domain'); ?>/payroll_setup/payroll_calendar/ajax_get_payroll_calendar",
+			data: {
+				pg_id: pg_id,
+				year: year,
+				<?php echo itoken_name();?>: jQuery.cookie("<?php echo itoken_cookie(); ?>")
 			}
-		});
+		}).done(function(ret){
+			jQuery("#pc_table_div").html(ret);
+			// invoke date picker
+			jQuery( ".dp" ).datepicker();
+		});	
+	});
+	
+	// if changed
+	jQuery(document).on("change","#payroll_calendar_dialog .inner_div input[type='text']",function(){
+		jQuery(this).parents("tr:first").find(".is_changed").val(1);
 	});
 	
 });
