2ae0e8aea0f6c7c08843f5d3696d511c35a0e792
developerside <christophercuizons@gmail.com>
2014-01-27 10:52:02 +0800

    updated menu

--- application/views/content_holders/user_hr_owner_menu.php
+++ application/views/content_holders/user_hr_owner_menu.php
@@ -1,5 +1,5 @@
       <ul>
-      	<li><a href="/<?php echo $this->uri->segment(1);?>/dashboard/company_list">HOME</a></li>  
+      	<li><a href="/<?php echo $this->session->userdata("sub_domain");?>/dashboard/company_list">HOME</a></li>  
 		<li><a href="/<?php echo $this->uri->segment(1);?>/dashboard/company_list">PAYROLL</a></li>  
 		<li><a href="/<?php echo $this->uri->segment(1);?>/hr/emp_basic_information">EMPLOYEE</a></li>  
 		<li><a href="/<?php echo $this->uri->segment(1);?>/hr/approve_leave/lists">APPROVAL</a></li>  
